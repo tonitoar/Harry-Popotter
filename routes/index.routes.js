529de0c67af984bd6aef4e645b487d9a514c28fc
Toni Soler <tonisoler1986@gmail.com>
2023-05-15 17:36:59 +0200

    routes started

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,61 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+
+//MAIN
+router.get("/welcome", (req, res, next) => {
+  res.render("inside/main");
+});
+
+
+//SPELLS
+router.get("/spells", (req, res, next) => {
+  res.render("inside/spells");
+});
+
+//TEST-HOUSE
+router.get("/test-house", (req, res, next) => {
+  res.render("inside/test-house");
+});
+
+
+router.post('/test-house', (req, res, next) => {
+  res.render("inside/test-house");
+  })
+
+//PROFILE
+
+router.get("/profile", (req, res, next) => {
+      res.render("inside/profile");
+  });
+
+router.post('/profile', (req, res, next) => {
+  res.render("inside/profile");
+  })
+
+
+
+//LOBBY
+router.get("/combat/lobby", (req, res, next) => {
+      res.render("inside/combatLobby");
+  });
+
+
+
+
+//FIGHT
+router.get("/combat/fight", (req, res, next) => {
+          res.render("inside/combatFight");
+      });
+  
+
+router.get("/combat/fight", (req, res, next) => {
+      res.render("inside/combatFight");
+  });
+
+
+
+
 module.exports = router;
+
+
