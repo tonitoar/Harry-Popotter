619853f7f14b95cd71a99b566f63812e13425ade
Toni Soler <tonisoler1986@gmail.com>
2023-05-18 09:30:13 +0200

    bootstrap added

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ const name = "Toni"
 require("./config")(app);
 
 const capitalize = require("./utils/capitalize");
-const projectName = "HPQ&A";
+const projectName = "HARRY POTTER Q&A";
 
-app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
+app.locals.appTitle = `${capitalize(projectName)}`;
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
